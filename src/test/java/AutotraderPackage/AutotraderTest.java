0a0e71139a4aebda7d52dd7cd78fc3d2e944172f
javedchowdhury001 <javedchowdhury001@gmail.com>
2020-05-25 00:56:20 -0400

    add new line

--- src/test/java/AutotraderPackage/AutotraderTest.java
+++ src/test/java/AutotraderPackage/AutotraderTest.java
@@ -34,6 +34,7 @@ public class AutotraderTest extends MyUltilityClass{
 public void model() {
 	Select day = new Select(Driver.findElement(By.xpath("//select[@name='ModelCode']")));	
 	day.selectByVisibleText("2_SERIES");
+	System.out.println("Select the car model add new line");
 }
 
 public void Select() {
